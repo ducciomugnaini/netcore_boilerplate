c12202bad0bf25a834cfe986413b68777a710c4e
ducciomugnaini <duccio.mugnaini@gmail.com>
2022-11-02 18:47:01 +0100

    init repo

---
+++ NetCoreMVCBoilerPlate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NetCoreMVCBoilerPlate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
