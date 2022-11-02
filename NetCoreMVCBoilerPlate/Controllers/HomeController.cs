2bfc6e6de5bb8b947773900ccdbd6773235a63aa
ducciomugnaini <duccio.mugnaini@gmail.com>
2022-11-02 20:04:24 +0100

    init repo - dummy migration

--- NetCoreMVCBoilerPlate/Controllers/HomeController.cs
+++ NetCoreMVCBoilerPlate/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NetCoreMVCBoilerPlate.Models;
 using System.Diagnostics;
+using NetCoreMVCBoilerPlate.Data;
 
 namespace NetCoreMVCBoilerPlate.Controllers
 {
    public class HomeController : Controller
    {
       private readonly ILogger<HomeController> _logger;
+      private readonly ApplicationDbContext _dbContext;
 
-      public HomeController(ILogger<HomeController> logger)
+      public HomeController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
       {
          _logger = logger;
+         _dbContext = applicationDbContext;
       }
 
       public IActionResult Index()
       {
+         var dummyEntities= _dbContext.DummyEntities.ToList();
          return View();
       }
 
